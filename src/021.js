399635e9ef0bf908f6669f31501cf1f59324bf3d
waihoyu <13701343809@qq.com>
2018-11-07 01:57:30 +0800

    update

--- src/021.js
+++ src/021.js
@@ -2,10 +2,7 @@ var  http =  require("http");
 
 var server = http.createServer(function (req,res) {
     res.writeHead(200, {"Content-Type": "text/html;charset=UTF-8"})
-    res.end("<h1>好高兴</h1>")
+    res.end("<h1>好高兴H1</h1><div>wo 不知道</div>")
 });
 
 server.listen(3000,"127.0.0.1");
-
-
-
--- src/022.js
+++ src/022.js
@@ -2,10 +2,7 @@ var  http =  require("http");
 
 var server = http.createServer(function (req,res) {
     res.writeHead(200, {"Content-Type": "text/html;charset=UTF-8"})
-    res.end("<h1>好高兴</h1>")
+    res.end("<h1>好高兴H1</h1>")
 });
 
 server.listen(3000,"127.0.0.1");
-
-
-
